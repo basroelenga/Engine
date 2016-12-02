6290bcf7c3731b0058564785b08072057f9adc5d
Bas <basroelenga@hotmail.com>
2016-12-02 22:26:06 +0100

    Add documentation

--- src/input/KeyboardInput.java
+++ src/input/KeyboardInput.java
@@ -14,13 +14,34 @@ public class KeyboardInput {
 		return glfwGetKey(Engine.window, (int) key.charAt(0));
 	}
 	
+	/**
+	 * Returns boolean state of key, only work with ASCII symbols.
+	 * @param key Key to check.
+	 * @return State of key.
+	 */
 	public static boolean isPressed(String key)
 	{
 		
 		// Return false if not pressed
 		boolean isPressed = false;
 		
-		if(glfwGetKey(Engine.window, (int) key.charAt(0)) == GLFW_PRESS) isPressed = true;
+		if(glfwGetKey(Engine.window, (int) key.charAt(0)) == GLFW_PRESS && key.length() == 1) isPressed = true;
+		return isPressed;
+	}
+	
+	/**
+	 * Returns boolean state of key, this should be used for non-ASCII symbols.
+	 * Example for input key is "GLFW_KEY_TAB" for TAB.
+	 * @param key Key to check.
+	 * @return State of key.
+	 */
+	public static boolean isPressed(int key)
+	{
+		
+		// Return false if not pressed
+		boolean isPressed = false;
+		
+		if(glfwGetKey(Engine.window, key) == GLFW_PRESS) isPressed = true;
 		return isPressed;
 	}
 }
