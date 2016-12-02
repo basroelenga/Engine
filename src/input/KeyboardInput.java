bec42e6d148ee77fd5ac6285d9046b13e53d47ae
Bas <basroelenga@hotmail.com>
2016-12-02 22:16:33 +0100

    Add press function for keyboard

--- src/input/KeyboardInput.java
+++ src/input/KeyboardInput.java
@@ -1,6 +1,7 @@
 package input;
 
 import static org.lwjgl.glfw.GLFW.glfwGetKey;
+import static org.lwjgl.glfw.GLFW.GLFW_PRESS;
 
 import engine.Engine;
 
@@ -11,5 +12,15 @@ public class KeyboardInput {
 	public static int getState(String key) {
 		
 		return glfwGetKey(Engine.window, (int) key.charAt(0));
+	}
+	
+	public static boolean isPressed(String key)
+	{
+		
+		// Return false if not pressed
+		boolean isPressed = false;
+		
+		if(glfwGetKey(Engine.window, (int) key.charAt(0)) == GLFW_PRESS) isPressed = true;
+		return isPressed;
 	}
 }
