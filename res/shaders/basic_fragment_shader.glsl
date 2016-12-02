db12c165a30f653b1f436c5ff95427d60dcdd0b7
Bas <basroelenga@hotmail.com>
2016-12-02 23:47:56 +0100

    Alpha level support

--- res/shaders/basic_fragment_shader.glsl
+++ res/shaders/basic_fragment_shader.glsl
@@ -1,10 +1,10 @@
 #version 330 core
 
-uniform vec4 rgbColor;
+uniform vec4 rgbaColor;
 
 layout(location = 0) out vec4 fragColor;
 
 void main()
 {
-	fragColor = vec4(rgbColor);
+	fragColor = vec4(rgbaColor);
 }
